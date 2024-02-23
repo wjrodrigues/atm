7a1f7cf4c5c9be32006fc2812da2c729ef479bd3
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-02-22 22:09:46 -0300

    chore: add INDEX on Vault

--- lib/model/vault.rb
+++ lib/model/vault.rb
@@ -3,6 +3,8 @@
 class Vault
   attr_accessor :bills
 
+  INDEX = { ten: 10, twenty: 20, fifty: 50, hundred: 100 }.freeze
+
   private :bills=, :bills
 
   def initialize
--- spec/lib/model/vault_spec.rb
+++ spec/lib/model/vault_spec.rb
@@ -4,6 +4,11 @@ require './lib/model/vault'
 require './spec/spec_helper'
 
 RSpec.describe Vault do
+  describe 'constants' do
+    it { expect(described_class::INDEX).to eq({ ten: 10, twenty: 20, fifty: 50, hundred: 100 }) }
+    it { expect(described_class::INDEX).to be_frozen }
+  end
+
   describe '#update!' do
     context 'when some notes are updated' do
       it 'returns updated vault' do
